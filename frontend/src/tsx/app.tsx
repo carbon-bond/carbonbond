a01d7797febaff93bae3bfc81311597d20aff751
MROS <yc1043@gmail.com>
2019-05-27 23:16:42 +0800

    設定 lint

---
+++ frontend/src/tsx/app.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+function App() {
+	return (
+		<div>金剛、石墨，參見！</div>
+	);
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,21 @@ extern crate env_logger;
 use actix_web::{server, App, HttpRequest, fs};
 use actix_web::middleware::Logger;
 
-fn index(_req: &HttpRequest) -> &'static str {
-    "金剛、石墨，參見！"
-}
-
 fn main() {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     server::new(|| vec![
-        App::new().prefix("/yo").resource("/", |r| r.f(index))
-            .middleware(Logger::default())
-            .middleware(Logger::new("%a %{User-Agent}i")),
+        // App::new().prefix("/yo").resource("/", |r| r.f(index))
+        //     .middleware(Logger::default())
+        //     .middleware(Logger::new("%a %{User-Agent}i")),
         App::new().handler(
-                "/app",
-                fs::StaticFiles::new("./frontend/static/")
+                "/",
+                fs::StaticFiles::new("./frontend/dist/")
                     .unwrap()
-                    .show_files_listing()),
+                    .show_files_listing())
+                    .middleware(Logger::default())
+                    .middleware(Logger::new("%a %{User-Agent}i")),
         ])
         .bind("127.0.0.1:8080")
         .unwrap()
