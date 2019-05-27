9f841a776be46eb516b70371c0541fae7fdf1e7a
MROS <yc1043@gmail.com>
2019-05-27 21:11:24 +0800

    伺服器動起來

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate actix_web;
+use actix_web::{server, App, HttpRequest};
+
+fn index(_req: &HttpRequest) -> &'static str {
+    "金剛、石墨，參見！"
+}
+
 fn main() {
-    println!("金剛、石墨，參見！");
+    server::new(|| App::new().resource("/", |r| r.f(index)))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
 }
