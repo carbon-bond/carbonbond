71f839015933d7096c280da80568c72000522868
MROS <yc1043@gmail.com>
2019-05-27 21:47:58 +0800

    靜態檔案 serve 起來

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 extern crate actix_web;
-use actix_web::{server, App, HttpRequest};
+extern crate env_logger;
+
+use actix_web::{server, App, HttpRequest, fs};
+use actix_web::middleware::Logger;
 
 fn index(_req: &HttpRequest) -> &'static str {
     "金剛、石墨，參見！"
 }
 
 fn main() {
-    server::new(|| App::new().resource("/", |r| r.f(index)))
-        .bind("127.0.0.1:8088")
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    server::new(|| vec![
+        App::new().prefix("/yo").resource("/", |r| r.f(index))
+            .middleware(Logger::default())
+            .middleware(Logger::new("%a %{User-Agent}i")),
+        App::new().handler(
+                "/app",
+                fs::StaticFiles::new("./frontend/static/")
+                    .unwrap()
+                    .show_files_listing()),
+        ])
+        .bind("127.0.0.1:8080")
         .unwrap()
         .run();
 }
