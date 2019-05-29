d8c3e3db80a1d55c375f3a6569f31749406f1c53
DavidYo <lturtsamuel@gmail.com>
2019-05-30 03:32:20 +0800

    前端改為函數寫法 準備使用 React 最勁爆的新功能

--- frontend/src/tsx/app.tsx
+++ frontend/src/tsx/app.tsx
@@ -15,29 +15,25 @@ import { LoginContext, Login } from "./types";
 import { Component } from "./component";
 import { RegisterPage } from "./register_page";
 
-class App extends React.Component<{}, { login: boolean, user_id: string|null }> {
-	constructor(props: {}) {
-		super(props);
-		this.state = {
-			login: false,
-			user_id: null,
-		};
-	}
-	setLoginState(user_id: string) {
-		if(this.state.login) {
+type LoginState = { login: true, user_id: string } | { login: false };
+
+function App() {
+	let [login_state, setLoginState] = React.useState<LoginState>({ login: false });
+	function setLoginUI(user_id: string) {
+		if(login_state.login) {
 			throw "已登入的狀況下設置登入";
 		} else {
-			this.setState({ login: true, user_id });
+			setLoginState({ login: true, user_id });
 		}
 	}
-	unsetLoginState() {
-		if(this.state.login) {
-			this.setState({ login: false, user_id: null });
+	function unsetLoginUI() {
+		if(login_state.login) {
+			setLoginState({ login: false });
 		} else {
 			throw "未登入的狀況下還想登出";
 		}
 	}
-	renderContent() {
+	function renderContent() {
 		return <div>
 			{/* HEADER */}
 			<Router>
@@ -54,10 +50,7 @@ class App extends React.Component<{}, { login: boolean, user_id: string|null }> 
 							{/* <button styleName="pure-button">local</button> */}
 							<div className="red">red global</div>
 							<div styleName="red">red local</div>
-							<Component
-								setLoginState={this.setLoginState.bind(this)}
-								unsetLoginState={this.unsetLoginState.bind(this)}
-							></Component>
+							<Component></Component>
 						</div>
 					)}/>
 					<Route path="/app/register/:invite_code"
@@ -67,24 +60,21 @@ class App extends React.Component<{}, { login: boolean, user_id: string|null }> 
 			</Router>
 		</div>;
 	}
-	render() {
-		let context: LoginContext = {
-			login: false,
-			setLogin: this.setLoginState.bind(this)
+	let context: LoginContext = {
+		login: false,
+		setLogin: setLoginUI
+	};
+	if(login_state.login) {
+		context = {
+			...login_state,
+			unsetLogin: unsetLoginUI
 		};
-		if(this.state.login && this.state.user_id) {
-			context = {
-				login: true,
-				user_id: this.state.user_id,
-				unsetLogin: this.unsetLoginState.bind(this)
-			};
-		}
-		return (
-			<Login.Provider value={context}>
-				{this.renderContent()}
-			</Login.Provider>
-		);
 	}
+	return (
+		<Login.Provider value={context}>
+			{renderContent()}
+		</Login.Provider>
+	);
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
--- frontend/src/tsx/register_page.tsx
+++ frontend/src/tsx/register_page.tsx
@@ -1,31 +1,27 @@
 import * as React from "react";
 
-import { LoginContext, Login, RouteComponentProps } from "./types";
+import { Login, RouteComponentProps } from "./types";
 
 type Props = RouteComponentProps<{ invite_code: string }>;
 
-export class RegisterPage extends React.Component<Props, {}, LoginContext> {
-	getInviteCode() {
-		let code = this.props.match.params.invite_code;
+export function RegisterPage(props: Props) {
+	let getInviteCode = React.useCallback(() => {
+		let code = props.match.params.invite_code;
 		if (typeof code == "string") {
 			return code;
 		} else {
 			throw "找不到邀請碼";
 		}
-	}
-	componentDidMount() {
-		// let code = this.getInviteCode();
+	}, [props.match.params.invite_code]);
+	let context = React.useContext(Login);
+	React.useEffect(() => {
+		let code = getInviteCode();
 		// 問後端此邀請碼的信箱
-		console.log(this.props);
-		if (this.context.login) {
+		console.log(context, code);
+		if (context.login) {
 			// 跳轉回首頁
-			this.props.history.replace("/app");
+			props.history.replace("/app");
 		}
-	}
-	render() {
-		return <div>
-			註冊頁
-		</div>;
-	}
+	}, [getInviteCode]);
+	return <div> 註冊頁 </div>;
 }
-RegisterPage.contextType = Login;
