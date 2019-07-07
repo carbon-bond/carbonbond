a42e30e347a19e849512c194de0a81597edc374d
MROS <yc1043@gmail.com>
2019-07-07 21:11:35 +0800

    改掉 db-tool 全部架構

--- src/db_tool.rs
+++ src/db_tool.rs
@@ -1,124 +1,116 @@
-use std::io::stdin;
+use std::io::{stdin, stdout};
+use std::io::Write;
 
 use carbonbond::user::{email, signup};
 use carbonbond::forum;
 use carbonbond::party;
 use carbonbond::db;
 
-use db::models::*;
-use db::schema::users::dsl::*;
-use diesel::prelude::*;
+static HELP_MSG: &'static str = "
+<> 表示必填， [] 表示選填
 
-fn main() -> Result<(), failure::Error> {
-    println!("碳鍵 - 資料庫管理介面");
-    let db_conn = db::connect_db();
-    let mut opt = 1;
-    let p1 = "[0] 結束程式";
-    let p2 = "[1] 新增使用者";
-    let p3 = "[2] 檢視使用者名單";
-    let p4 = "[3] 寄出邀請信";
-    let p5 = "[4] 新增看板";
-    let p6 = "[5] 新增政黨";
-    while opt != 0 {
-        println!("{}", p1);
-        println!("{}", p2);
-        println!("{}", p3);
-        println!("{}", p4);
-        println!("{}", p5);
-        println!("{}", p6);
-        let mut buff = String::new();
+add 子命令：
+    add user <信箱地址> <使用者名稱> <密碼>
+    add party <政黨名> [看板名]
+        如果不填看板名，會建立流亡政黨
+    add board <流亡政黨 id> <看板名>
+as 子命令（尚未實作）：
+    as <使用者名稱>
+        db-tool 會記住你的身份，在執行創黨/發文等等功能時自動填入
+invite 子命令：
+    invite <信箱地址>
+";
+
+fn match_subcommand() -> Result<(String, Vec<String>), failure::Error> {
+    print!("> ");
+    stdout().flush()?;
+    let mut buff = String::new();
+    loop {
         stdin().read_line(&mut buff)?;
-        if let Ok(_opt) = buff.replace("\n", "").parse::<u8>() {
-            opt = _opt;
-            if opt == 0 {
-                break;
-            } else if opt == 1 {
-                loop {
-                    println!("> {}", p2);
-                    println!("> 請輸入 信箱 名字 密碼，或輸入空白行回到選單");
-                    buff.clear();
-                    stdin().read_line(&mut buff)?;
-                    let words: Vec<&str> = buff.split_whitespace().collect();
-                    if words.len() == 0 {
-                        break;
-                    }
-                    if words.len() != 3 {
-                        println!("輸入格式錯誤");
-                    } else {
-                        signup::create_user(&db_conn, words[0], words[1], words[2]);
-                        println!("成功新增使用者：{}", words[0]);
-                    }
-                }
-            } else if opt == 2 {
-                let results = users
-                    //.filter(id.eq("石墨"))
-                    .load::<User>(&db_conn)
-                    .expect("取使用者失敗");
-                for user in results {
-                    println!(
-                        "id: {} email: {} 推薦額度: {}",
-                        user.id, user.email, user.invitation_credit
-                    );
-                }
-                println!("\n");
-            } else if opt == 3 {
-                loop {
-                    println!("> {}", p4);
-                    println!("> 請輸入欲邀請人的信箱，或輸入空白行回到選單");
-                    buff.clear();
-                    stdin().read_line(&mut buff)?;
-                    let words: Vec<&str> = buff.split_whitespace().collect();
-                    if words.len() == 0 {
-                        break;
-                    }
-                    if words.len() != 1 {
-                        println!("輸入格式錯誤");
-                    } else {
-                        let invite_code = signup::create_invitation(&db_conn, None, words[0])
-                            .expect("無法建立邀請");
-                        email::send_invite_email(None, &invite_code, words[0])
-                            .expect("寄送邀請信失敗");
-                    }
-                }
-            } else if opt == 4 {
-                println!("> {}", p5);
-                println!("> 請輸入看板名，或輸入空白行回到選單");
-                buff.clear();
-                stdin().read_line(&mut buff)?;
-                let words: Vec<&str> = buff.split_whitespace().collect();
-                if words.len() == 0 {
-                    break;
-                }
-                if words.len() != 1 {
-                    println!("輸入格式錯誤");
-                } else {
-                    forum::create_board(&db_conn, -1, words[0])?;
-                    println!("成功新增看板：{}", words[0]);
-                }
-            } else if opt == 5 {
-                println!("> {}", p6);
-                println!("> 請輸入 黨名 [所屬看板]，或輸入空白行回到選單");
-                buff.clear();
-                stdin().read_line(&mut buff)?;
-                let words: Vec<&str> = buff.split_whitespace().collect();
-                match words.len() {
-                    0 => break,
-                    1 => {
-                        party::create_party(&db_conn, None, words[0])?;
-                        println!("成功新增政黨：{}", words[0]);
-                    }
-                    2 => {
-                        party::create_party_with_board_name(&db_conn, Some(words[1]), words[0])?;
-                        println!("成功新增 {}版 政黨：{}", words[1], words[0]);
-                    }
-                    _ => println!("輸入格式錯誤"),
-                }
-            } else {
-                println!("請輸入範圍內的正整數");
-            }
-        } else {
-            println!("請輸入範圍內的正整數");
+        let words: Vec<String> = buff.split_whitespace().map(|w| w.to_owned()).collect();
+        if words.len() == 0 {
+            continue;
         }
+        return Ok((words[0].clone(), words[1..].to_vec()));
+    }
+}
+
+fn add_something(args: &Vec<String>) -> Result<(), failure::Error> {
+    if args.len() < 2 {
+        return Err(failure::format_err!("add 參數數量錯誤"));
+    }
+    let something = args[0].clone();
+    let sub_args = args[1..].to_vec();
+    match something.as_ref() {
+        "user" => add_user(&sub_args),
+        "party" => add_party(&sub_args),
+        "board" => add_board(&sub_args),
+        other => Err(failure::format_err!("無法 add {}", other)),
+    }
+}
+
+fn add_user(args: &Vec<String>) -> Result<(), failure::Error> {
+    if args.len() != 3 {
+        return Err(failure::format_err!("add user 參數數量錯誤"));
+    }
+    let (email, id, password) = (&args[0], &args[1], &args[2]);
+    // TODO: create_user 內部要做錯誤處理
+    signup::create_user(&db::connect_db(), email, id, password);
+    Ok(())
+}
+
+fn add_party(args: &Vec<String>) -> Result<(), failure::Error> {
+    if args.len() == 1 {
+        let party = &args[0];
+        let id = party::create_party(&db::connect_db(), None, party)?;
+        println!("成功建立政黨，id = {}", id);
+        Ok(())
+    } else if args.len() == 2 {
+        let (party, board) = (&args[0], &args[1]);
+        let id = party::create_party_with_board_name(&db::connect_db(), Some(board), party)?;
+        println!("成功建立政黨，id = {}", id);
+        Ok(())
+    } else {
+        Err(failure::format_err!("add party 參數數量錯誤"))
+    }
+}
+
+fn add_board(args: &Vec<String>) -> Result<(), failure::Error> {
+    if args.len() != 2 {
+        return Err(failure::format_err!("add board 參數數量錯誤"));
+    }
+    let (party, board) = (args[0].parse::<i64>()?, &args[1]);
+    let id = forum::create_board(&db::connect_db(), party, board)?;
+    println!("成功建立看板，id = {}", id);
+    Ok(())
+}
+
+fn invite(args: &Vec<String>) -> Result<(), failure::Error> {
+    let invite_code = signup::create_invitation(&db::connect_db(), None, &args[0])?;
+    email::send_invite_email(None, &invite_code, &args[0])?;
+    Ok(())
+}
+
+fn exec_command() -> Result<(), failure::Error> {
+    let (subcommand, args) = match_subcommand()?;
+    match subcommand.as_ref() {
+        "help" => println!("{}", HELP_MSG),
+        "add" => add_something(&args)?,
+        "as" => println!("as 子指令未實作"),
+        "invite" => invite(&args)?,
+        other => println!("無 {} 指令", other),
     }
     Ok(())
+}
+
+fn main() {
+    println!("碳鍵 - 資料庫管理介面\n使用 help 查詢指令");
+    loop {
+        match exec_command() {
+            Err(error) => {
+                println!("執行失敗： {}", error);
+            }
+            _ => {}
+        }
+    }
 }
