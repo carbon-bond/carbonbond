04c3802943a32e99d576271fe6352397c6c28951
jerry73204 <jerry73204@gmail.com>
2019-07-22 18:40:38 +0800

    從單獨檔案讀取 mailgun API key

--- src/config.rs
+++ src/config.rs
@@ -1,24 +1,77 @@
 use std::io::prelude::*;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fs::File;
 use serde::{Serialize, Deserialize};
 use failure::Fallible;
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct RawConfig {
+    pub server: RawServerConfig,
+    pub database: RawDatabaseConfig,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RawServerConfig {
+    pub address: String,
+    pub port: u64,
+    pub mailgun_key_file: PathBuf,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RawDatabaseConfig {
+    pub url: String,
+}
+
+#[derive(Debug)]
 pub struct Config {
     pub server: ServerConfig,
     pub database: DatabaseConfig,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct ServerConfig {
     pub address: String,
     pub port: u64,
+    pub mailgun_api_key: Vec<u8>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug)]
 pub struct DatabaseConfig {
     pub url: String,
+}
+
+impl From<RawConfig> for Fallible<Config> {
+    fn from(orig: RawConfig) -> Fallible<Config> {
+        Ok(Config {
+            server: Fallible::<ServerConfig>::from(orig.server)?,
+            database: Fallible::<DatabaseConfig>::from(orig.database)?,
+        })
+    }
+}
+
+impl From<RawServerConfig> for Fallible<ServerConfig> {
+    fn from(orig: RawServerConfig) -> Fallible<ServerConfig> {
+        let mailgun_api_key = {
+            let mut buf = vec![];
+            let mut file = File::open(orig.mailgun_key_file)?;
+            file.read_to_end(&mut buf)?;
+            buf
+        };
+
+        Ok(ServerConfig {
+            address: orig.address,
+            port: orig.port,
+            mailgun_api_key,
+        })
+    }
+}
+
+impl From<RawDatabaseConfig> for Fallible<DatabaseConfig> {
+    fn from(orig: RawDatabaseConfig) -> Fallible<DatabaseConfig> {
+        Ok(DatabaseConfig {
+            url: orig.url,
+        })
+    }
 }
 
 pub fn load_config<P: AsRef<Path>>(path: P) -> Fallible<Config> {
@@ -31,6 +84,7 @@ pub fn load_config<P: AsRef<Path>>(path: P) -> Fallible<Config> {
         content
     };
 
-    let config: Config = toml::from_str(&content)?;
+    let raw_config: RawConfig = toml::from_str(&content)?;
+    let config = Fallible::<Config>::from(raw_config)?;
     Ok(config)
 }
