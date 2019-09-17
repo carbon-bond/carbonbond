28dd42eef349b25398748cec4b3cd8a2c60c3e30
Bunch Lin <shex.ck@gmail.com>
2019-09-17 11:01:16 +0800

    fix signup_check() appearance

--- frontend/src/tsx/signup_page.tsx
+++ frontend/src/tsx/signup_page.tsx
@@ -17,24 +17,24 @@ export function SignupPage(props: Props): JSX.Element {
 	let repeated_password = useInputValue('').input_props;
 	let [invitation, setInvitation] = React.useState<Invitation | null>(null);
 
+	async function signup_request(code: string, name: string, password: string): Promise<{}> {
+		try {
+			await ajaxOperation.Signup({ code, name, password });
+			props.history.push('/app/');
+			toast('註冊成功');
+
+		} catch (err) {
+			toast.error(extractErrMsg(err));
+		}
+		return {};
+	}
+
 	function signup_check(code: string, name: string, password: string, repeated_password: string): {} {
 		if (password === repeated_password){
 			signup_request(code, name, password);
 		}
-		else{
+		else {
 			toast.error('密碼與確認密碼不一致！');
-		} 
-		return {};
-	}
-
-	async function signup_request(code: string, name: string, password: string): Promise<{}> {
-		try {
-			await ajaxOperation.Signup({ code, name, password });
-			props.history.push('/app/'); 
-			toast('註冊成功');
-			
-		} catch (err) {
-			toast.error(extractErrMsg(err));
 		}
 		return {};
 	}
@@ -51,7 +51,7 @@ export function SignupPage(props: Props): JSX.Element {
 	React.useEffect(() => {
 		let invite_code = getInviteCode();
 		fetchInvitation(invite_code).then(invitation => {
-			setInvitation(invitation)
+			setInvitation(invitation);
 		}).catch(err => console.log(err));
 	}, [getInviteCode]);
 
@@ -62,16 +62,16 @@ export function SignupPage(props: Props): JSX.Element {
 					<div styleName="counter">Sorry! {invitation.inviteeEmail} 此信箱已經註冊過了</div>
 				</div>
 			</div>;
-		}else{
+		} else {
 			return <div styleName="signupPage">
-			<div styleName="signupForm">
-				<div styleName="counter">這封邀請信是{invitation.inviterName}寄給你的</div>
-				<div styleName="counter">他說：{invitation.words}</div>
-				<div styleName="counter">你的email是：{invitation.inviteeEmail}</div>
-				<input styleName="username" type="text" placeholder="使用者名稱" {...name} autoFocus />
-				<input styleName="password" type="password" placeholder="密碼" {...password} autoFocus />
-				<input styleName="password" type="password" placeholder="確認密碼" {...repeated_password} autoFocus />
-				<button onClick={ () => signup_check(invitation!.code, name.value, password.value, repeated_password.value) }>註冊帳號</button>
+				<div styleName="signupForm">
+					<div styleName="counter">這封邀請信是{invitation.inviterName}寄給你的</div>
+					<div styleName="counter">他說：{invitation.words}</div>
+					<div styleName="counter">你的email是：{invitation.inviteeEmail}</div>
+					<input styleName="username" type="text" placeholder="使用者名稱" {...name} autoFocus />
+					<input styleName="password" type="password" placeholder="密碼" {...password} autoFocus />
+					<input styleName="password" type="password" placeholder="確認密碼" {...repeated_password} autoFocus />
+					<button onClick={ () => signup_check(invitation!.code, name.value, password.value, repeated_password.value) }>註冊帳號</button>
 				</div>
 			</div>;
 		}
