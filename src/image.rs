7b435715cb311381101d9b3d250961be26ef9a29
MROS <yc1043@gmail.com>
2019-10-22 00:21:28 +0800

    增加無大頭貼時的替代圖片

--- src/image.rs
+++ src/image.rs
@@ -1,6 +1,25 @@
 use actix_web::{HttpResponse, web};
 use actix_web::dev::Body;
 use crate::{user, db};
+use std::fs::File;
+use std::io::Read;
+
+
+pub fn no_avatar() -> HttpResponse {
+    let f = File::open("./frontend/static/img/no-avatar.png");
+    match f {
+        Ok(mut f) => {
+            let mut data = Vec::new();
+            match f.read_to_end(&mut data) {
+                Ok(_) => {
+                    HttpResponse::Found().body(Body::from_slice(&data))
+                },
+                Err(_) => HttpResponse::InternalServerError().finish()
+            }
+        },
+        Err(_) => HttpResponse::InternalServerError().finish()
+    }
+}
 
 pub fn avatar(path: web::Path<(String,)>) -> HttpResponse {
     let user_name = &path.0;
@@ -23,7 +42,7 @@ pub fn avatar(path: web::Path<(String,)>) -> HttpResponse {
                 let response = HttpResponse::Found().body(Body::from_slice(&image));
                 return response;
             } else {
-                return HttpResponse::NotFound().finish();
+                return no_avatar();
             }
         }
     }
