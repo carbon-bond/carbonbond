b861e00a1ed1bf4ad8008007ac6c0f780e39de9b
why8ther <lturtsamuel@gmail.com>
2020-07-31 10:27:35 +0800

    rust github action 中加入 postgres 容器設置

--- frontend/src/tsx/global_state/subscribed_boards.tsx
+++ frontend/src/tsx/global_state/subscribed_boards.tsx
@@ -4,35 +4,35 @@ import { Map } from 'immutable';
 import { BoardOverview } from '../../ts/api/api_trait';
 
 function useSubecribedBoardsState(): {
-    subscribed_boards: Map<number, BoardOverview>,
-    subscribe: (board: BoardOverview) => void,
-    unsubscribe: (id: number) => void,
-    load: (boards: BoardOverview[]) => void
+	subscribed_boards: Map<number, BoardOverview>,
+	subscribe: (board: BoardOverview) => void,
+	unsubscribe: (id: number) => void,
+	load: (boards: BoardOverview[]) => void
 } {
-    let [data, setData] = useState(Map<number, BoardOverview>());
-    let subscribe = (board: BoardOverview) => {
-        setData(m => {
-            return m.set(board.id, board);
-        });
-    }
-    let unsubscribe = (id: number) => {
-        setData(m => {
-            return m.remove(id);
-        });
-    }
-    let load = (boards: BoardOverview[]) => {
-        let list: [number, BoardOverview][] = []
-        for (const board of boards) {
-            list.push([board.id, board]);
-        }
-        setData(Map(list));
-    }
-    return {
-        subscribed_boards: data,
-        unsubscribe,
-        subscribe,
-        load,
-    }
+	let [data, setData] = useState(Map<number, BoardOverview>());
+	let subscribe = (board: BoardOverview) => {
+		setData(m => {
+			return m.set(board.id, board);
+		});
+	}
+	let unsubscribe = (id: number) => {
+		setData(m => {
+			return m.remove(id);
+		});
+	}
+	let load = (boards: BoardOverview[]) => {
+		let list: [number, BoardOverview][] = []
+		for (const board of boards) {
+			list.push([board.id, board]);
+		}
+		setData(Map(list));
+	}
+	return {
+		subscribed_boards: data,
+		unsubscribe,
+		subscribe,
+		load,
+	}
 }
 
 export const SubscribedBoardsState = createContainer(useSubecribedBoardsState);
