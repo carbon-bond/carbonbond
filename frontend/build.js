533dd161128036677d1458375a2be821cfcd689b
MROS <yc1043@gmail.com>
2020-09-11 15:56:46 +0800

    徹底驅除 graphql

--- frontend/build.js
+++ frontend/build.js
@@ -1,8 +1,5 @@
 const webpack = require('webpack');
 const config = require('./webpack.config.js');
-const { execSync } = require('child_process');
-const watch = require('node-watch');
-require('colors');
 
 const argv = require('minimist')(process.argv.slice(2));
 
@@ -21,30 +18,13 @@ const handler = (err, stats) => {
 
 const compiler = webpack(config);
 
-function api_codegen() {
-	try {
-		execSync('yarn api-codegen', { stdio: 'inherit' });
-		console.log('graphql 編譯完成'.green);
-	} catch (err) {
-		console.log('graphql 編譯錯誤'.red);
-	}
-}
-
 if (argv['watch']) {
-	// api codegen
-	api_codegen();
-	watch(`${__dirname}/../api/api.gql`, api_codegen);
-	watch(`${__dirname}/operation`, api_codegen);
-
 	// webpack
 	compiler.watch({
 		aggregateTimeout: 300,
 		poll: undefined
 	}, handler);
 } else {
-	// api codegen
-	api_codegen();
-
 	// webpack
 	compiler.run(handler);
 }
