191a92a236864cde2bc659431f50ba8db24f5b63
fro11o <tcps195283@gmail.com>
2020-12-25 22:17:49 +0800

    修正 mig 錯誤

--- migrations/20200719123355_create_db.sql
+++ migrations/20200719123355_create_db.sql
@@ -176,7 +176,7 @@ CREATE TABLE favorite_articles (
   id bigserial PRIMARY KEY,
   user_id bigint REFERENCES users (id) NOT NULL,
   article_id bigint REFERENCES articles (id) NOT NULL,
-  create_time timestamptz NOT NULL DEFAULT NOW()
+  create_time timestamptz NOT NULL DEFAULT NOW(),
   UNIQUE (user_id, article_id)
 );
 
