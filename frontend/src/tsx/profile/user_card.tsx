6ef99636854f746c8bd0379e7e8c13892830fc53
MROS <yc1043@gmail.com>
2021-02-06 14:18:30 +0800

    修正人際關係中大頭貼導致的不對齊問題：先都弄成正方形

--- frontend/src/tsx/profile/user_card.tsx
+++ frontend/src/tsx/profile/user_card.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { UserMini } from '../../ts/api/api_trait';
-import { Avatar } from './avatar';
 import '../../css/user_card.css';
 
 function UserCard(props: { user: UserMini }): JSX.Element {
@@ -10,7 +9,7 @@ function UserCard(props: { user: UserMini }): JSX.Element {
 	return (
 		<div styleName="userContainer">
 			<div styleName="userAvatar">
-				<Avatar is_me={false} name={props.user.user_name} />
+				<img src={`/avatar/${props.user.user_name}`} />
 			</div>
 			<div styleName="userDetail">
 				<div styleName="userName">{props.user.user_name}</div>
