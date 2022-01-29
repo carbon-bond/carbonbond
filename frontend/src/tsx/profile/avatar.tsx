e60e7fe29d6528d7f6e472e1fd6c88e5a7652a10
fro11o <tcps195283@gmail.com>
2022-01-29 23:26:51 +0800

    可剪裁上傳的大頭貼

--- frontend/src/tsx/profile/avatar.tsx
+++ frontend/src/tsx/profile/avatar.tsx
@@ -2,14 +2,25 @@ import * as React from 'react';
 import ReactModal from 'react-modal';
 import { API_FETCHER, unwrap } from '../../ts/api/api';
 import { toastErr } from '../utils';
+import Cropper from 'react-easy-crop';
+import { Point, Area } from 'react-easy-crop/types';
 
 import style from '../../css/avatar.module.css';
 
-// TODO: 可剪裁非正方形的圖片
 function EditAvatar(props: { name: string }): JSX.Element {
 	// const { user_state } = UserState.useContainer();
 	const [is_editing, setIsEditing] = React.useState<boolean>(false);
 	const [preview_data, setPreviewData] = React.useState<string | null>(null);
+	const [crop, setCrop] = React.useState<Point>({ x: 0, y: 0 });
+	const [cropped_area, setCroppedArea] = React.useState<Area>({x: 0, y: 0, height: 0, width: 0});
+	const [zoom, setZoom] = React.useState(1);
+
+	const onCropComplete = React.useCallback(
+		(_cropped_area: Area, cropped_area_pixels: Area) => {
+			setCroppedArea(cropped_area_pixels);
+		},
+		[]
+	);
 
 	function chooseAvatar(e: React.ChangeEvent<HTMLInputElement>): void {
 		e.preventDefault();
@@ -31,11 +42,30 @@ function EditAvatar(props: { name: string }): JSX.Element {
 		return;
 	}
 
+	function CropAvatar(image_src: string, pixel_crop: Area): string {
+		let image = new Image();
+		image.src = image_src;
+		const canvas = document.createElement('canvas');
+		const ctx = canvas.getContext('2d');
+		if (!ctx) {
+			return '';
+		}
+		canvas.width = pixel_crop.width;
+		canvas.height = pixel_crop.height;
+		ctx.drawImage(image,
+					  pixel_crop.x, pixel_crop.y, pixel_crop.width, pixel_crop.height,
+					  0, 0, canvas.width, canvas.height);
+		return canvas.toDataURL('image/png');
+	}
+
 	async function uploadAvatar(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): Promise<{}> {
 		e.preventDefault();
 		try {
 			if (preview_data != null) {
-				unwrap(await API_FETCHER.userQuery.updateAvatar(preview_data.split(',')[1]));
+				let cropped_data = CropAvatar(preview_data, cropped_area);
+				if (cropped_data != '') {
+					unwrap(await API_FETCHER.userQuery.updateAvatar(cropped_data.split(',')[1]));
+				}
 			}
 			setIsEditing(false);
 			location.reload();
@@ -61,7 +91,17 @@ function EditAvatar(props: { name: string }): JSX.Element {
 			}} >
 			{
 				preview_data ?
-					<img src={preview_data} height="144" width="144"></img> :
+					<div className={style.cropper}>
+						<Cropper
+							image={preview_data}
+							crop={crop}
+							zoom={zoom}
+							aspect={3 / 3}
+							onCropChange={setCrop}
+							onCropComplete={onCropComplete}
+							onZoomChange={setZoom}
+						/>
+					</div> :
 					<div>出了些問題......</div>
 			}
 			<div className={style.buttonSet}>
