3e1c299e441d6a6800131a31e67527842f3d71e4
MROS <yc1043@gmail.com>
2022-03-01 21:51:44 +0800

    刪除力語言資料夾

--- force/rust/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod defs;
-pub mod error;
-pub mod instance_defs;
-pub mod lexer;
-pub mod parser;
-pub mod validate;
-
-pub use crate::defs::*;
-pub use crate::parser::{parse, parse_category};
--- force/typescript/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './src/instance_defs';
-export * from './src/defs';
-export * from './src/validate';
-export { parse, parse_category } from './src/parser';
