c4a640e700b96b997f4be65d09851e49bab9162c
MROS <yc1043@gmail.com>
2022-03-26 20:31:23 +0800

    根據當前協定(http 或 https)，發送 API / 建立 websocket

--- frontend/src/ts/utils.ts
+++ frontend/src/ts/utils.ts
@@ -1,25 +1,28 @@
+function fallbackCopyToClipboard(text_to_copy: string): Promise<void> {
+	console.warn('瀏覽器剪貼簿僅在 https 或 localhost 可用，現採用兼容方式複製');
+	// 创建text area
+	let text_area = document.createElement('textarea');
+	text_area.value = text_to_copy;
+	// 使text area不在viewport，同时设置不可见
+	text_area.style.position = 'fixed';
+	text_area.style.opacity = '0';
+	text_area.style.left = '-999999px';
+	text_area.style.top = '-999999px';
+	document.body.appendChild(text_area);
+	text_area.focus();
+	text_area.select();
+	return new Promise((res, rej) => {
+		// 执行复制命令并移除文本框
+		document.execCommand('copy') ? res() : rej();
+		text_area.remove();
+	});
+}
+
 export function copyToClipboard(text_to_copy: string): Promise<void> {
 	// navigator clipboard 需要https等安全上下文
 	if (navigator.clipboard) {
-		// navigator clipboard 向剪贴板写文本
 		return navigator.clipboard.writeText(text_to_copy);
 	} else {
-		console.warn('瀏覽器剪貼簿僅在 https 或 localhost 可用，現採用兼容方式複製');
-		// 创建text area
-		let text_area = document.createElement('textarea');
-		text_area.value = text_to_copy;
-		// 使text area不在viewport，同时设置不可见
-		text_area.style.position = 'absolute';
-		text_area.style.opacity = '0';
-		text_area.style.left = '-999999px';
-		text_area.style.top = '-999999px';
-		document.body.appendChild(text_area);
-		text_area.focus();
-		text_area.select();
-		return new Promise((res, rej) => {
-			// 执行复制命令并移除文本框
-			document.execCommand('copy') ? res() : rej();
-			text_area.remove();
-		});
+		return fallbackCopyToClipboard(text_to_copy);
 	}
 }
