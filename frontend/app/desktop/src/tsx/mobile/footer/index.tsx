0c66b97c9453b35acbebae7b1fa1cf187a6cbe82
MROS <yc1043@gmail.com>
2022-07-30 13:17:34 +0800

    足部可以開闔

--- frontend/app/desktop/src/tsx/mobile/footer/index.tsx
+++ frontend/app/desktop/src/tsx/mobile/footer/index.tsx
@@ -4,17 +4,49 @@ import { AllChatState, ChatData } from '../../global_state/chat';
 
 export function Footer(): JSX.Element {
 	const { all_chat } = AllChatState.useContainer();
-	// let [expanding, setExpanding] = React.useState<boolean>();
+	let [expanding, setExpanding] = React.useState<boolean | null>(null);
 	let chat_array: ChatData[] = Array.from(Object.values(all_chat.direct));
-	return <div className={style.footer}>
-		{
-			chat_array.filter(chat => chat.isUnread()).map(chat => {
-				return <div
-					key={chat.id}
-					className={`${style.avatar} ${style.unread}`}>
-					{chat.toAvatar(style.avatar)}
-				</div>;
-			})
+
+	function getPanelClassName(): string {
+		if (expanding == null) {
+			return style.panelInit;
+		} else if (expanding == true) {
+			return style.panelOpen;
+		} else {
+			return style.panelClose;
 		}
-	</div>;
+	}
+
+	function getFooterClassName(): string {
+		if (expanding == null) {
+			return style.footerInit;
+		} else if (expanding == true) {
+			return style.footerOpen;
+		} else {
+			return style.footerClose;
+		}
+	}
+
+	return <>
+		<div
+			className={`${style.footer} ${getFooterClassName()}`}
+		>
+			{
+				chat_array.filter(chat => chat.isUnread()).map(chat => {
+					return <div
+						key={chat.id}
+						onClick={() => {
+							console.log('click');
+							setExpanding(!expanding);
+						}}
+						className={`${style.avatar} ${style.unread}`}>
+						{chat.toAvatar(style.avatar)}
+					</div>;
+				})
+			}
+		</div >
+		<div className={`${style.panel} ${getPanelClassName()}`}>
+			面板
+		</div>
+	</>;
 }
