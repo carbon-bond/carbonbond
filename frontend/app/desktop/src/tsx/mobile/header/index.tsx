b0f7f27398e95eb04c74ba87b4a65c598510c9a8
MROS <yc1043@gmail.com>
2022-08-24 20:20:55 +0800

    建出行動版通知框架

--- frontend/app/desktop/src/tsx/mobile/header/index.tsx
+++ frontend/app/desktop/src/tsx/mobile/header/index.tsx
@@ -14,10 +14,10 @@ import { UserState } from '../../global_state/user';
 import { UserCard } from '../../profile/user_card';
 
 // 當左邊欄展開時，給原本的畫面加一層濾鏡
-function Filter(props: { setExpanding: (expanding: boolean) => void }): JSX.Element {
+function Filter(props: { setClose: () => void }): JSX.Element {
 	return <div
 		onClick={() => {
-			props.setExpanding(false);
+			props.setClose();
 		}}
 		className={panel_style.filter} >
 	</div>;
@@ -46,19 +46,19 @@ function LeftPanel(props: { setExpanding: (expanding: boolean) => void }): JSX.E
 				<PanelMenu option={option} toggleOption={toggleOption}/>
 				<PanelMain option={option} onLinkClick={() => { props.setExpanding(false); }}/>
 			</div>
-			<Filter setExpanding={props.setExpanding} />
+			<Filter setClose={() => props.setExpanding(false)} />
 		</>
 	);
 }
 
-function RightPanel(props: { setExpanding: (expanding: boolean) => void }): JSX.Element {
+function NavigationPanel(props: { setClose: () => void }): JSX.Element {
 	const { user_state } = UserState.useContainer();
 	if (!user_state.login) {
 		return <></>;
 	}
 	return (
 		<>
-			<div className={panel_style.rightPanel} onClick={() => props.setExpanding(false)}>
+			<div className={panel_style.rightPanel} onClick={() => props.setClose()}>
 				<div className={panel_style.userCardWrap}>
 					<UserCard user={user_state}/>
 				</div>
@@ -66,15 +66,47 @@ function RightPanel(props: { setExpanding: (expanding: boolean) => void }): JSX.
 					{Links()}
 				</div>
 			</div>
-			<Filter setExpanding={props.setExpanding} />
+			<Filter setClose={props.setClose} />
 		</>
 	);
+}
+
+function NotificationPanel(props: { setClose: () => void }): JSX.Element {
+	const { user_state } = UserState.useContainer();
+	if (!user_state.login) {
+		return <></>;
+	}
+	return (
+		<>
+			<div className={panel_style.rightPanel} onClick={() => props.setClose()}>
+				通知
+			</div>
+			<Filter setClose={props.setClose} />
+		</>
+	);
+}
+
+enum RightPanelState {
+	Navigation,
+	Notification,
+	None
+}
+
+function RightPanel(props: {panel_state: RightPanelState, setPanelState: (state: RightPanelState) => void }): JSX.Element {
+	switch (props.panel_state) {
+		case RightPanelState.Navigation:
+			return <NavigationPanel setClose={() => props.setPanelState(RightPanelState.None)}/>;
+		case RightPanelState.Notification:
+			return <NotificationPanel setClose={() => props.setPanelState(RightPanelState.None)}/>;
+		case RightPanelState.None:
+			return <></>;
+	}
 }
 
 function Header(): JSX.Element {
 	const { current_location } = LocationState.useContainer();
 	const [ expanding_left_panel, setExpandingLeftPanel ] = React.useState(false);
-	const [ expanding_right_panel, setExpandingRightPanel ] = React.useState(false);
+	const [ right_panel_state, setRightPanelState ] = React.useState(RightPanelState.None);
 	const [modal_status, setModalStatus] = React.useState<ModalStatus | null>(null);
 	const navigate = useNavigate();
 	const { user_state } = UserState.useContainer();
@@ -89,7 +121,7 @@ function Header(): JSX.Element {
 	return (
 		<div className={`header ${style.header}`}>
 			{ expanding_left_panel ? <LeftPanel setExpanding={setExpandingLeftPanel}/> : null }
-			{ expanding_right_panel ? <RightPanel setExpanding={setExpandingRightPanel}/> : null }
+			<RightPanel panel_state={right_panel_state} setPanelState={setRightPanelState}/>
 			<div className={style.container}>
 				<div className={style.leftSet} onClick={() => setExpandingLeftPanel(true)}>
 					☰
@@ -98,18 +130,22 @@ function Header(): JSX.Element {
 					<div className={style.location} style={{ fontSize: 14 }} onClick={routeToBoard}>{title}</div>
 				</div>
 				<div className={style.rightSet}>
-					<div className={style.wrap}>
-						{
-							user_state.login
-								? <img
-									onClick={() => setExpandingRightPanel(true)}
+					{
+						user_state.login
+							? <div className={style.wrap}>
+								<div className={style.icon}
+									onClick={() => setRightPanelState(RightPanelState.Notification)}>
+									🔔
+								</div>
+								<img
+									onClick={() => setRightPanelState(RightPanelState.Navigation)}
 									className={style.avatar}
 									src={`/avatar/${user_state.user_name}`} />
-								: user_state.fetching
-									? <></>
-									: <div className={style.login} onClick={() => setModalStatus(ModalStatus.Login)}>登入</div>
-						}
-					</div>
+							</div>
+							: user_state.fetching
+								? <></>
+								: <div className={style.login} onClick={() => setModalStatus(ModalStatus.Login)}>登入</div>
+					}
 				</div>
 				{ modal_status ? <LoginModal setModalStatus={setModalStatus} modal_status={modal_status}/> : null }
 			</div>
