6b8e76711d9c5ce4e4e0e2361322b7375fc35e46
MROS <yc1043@gmail.com>
2023-02-22 02:27:36 +0800

    使用 vitepress 建立文件網站

---
+++ docs-website/docs/.vitepress/config.ts
@@ -0,0 +1,30 @@
+export default {
+  title: '碳鍵',
+  description: '戰爭，一觸即發',
+  themeConfig: {
+	  logo: '/icon.svg',
+    nav: [
+      { text: '主站', link: 'https://carbonbond.cc' }
+    ],
+	  sidebar: [
+		  {
+        text: '概述',
+        items: [
+          { text: '碳鍵是什麼？', link: '/概述/碳鍵是什麼.html' }
+        ]
+      },
+		  {
+        text: '早期文件',
+        items: [
+          { text: '起源', link: '/起源.html' },
+          { text: '快速開始', link: '/快速開始.html' },
+          { text: 'docker啓動', link: '/docker啓動.html' },
+          { text: 'rust 設置', link: '/rust設置.html' },
+          { text: '前端設置', link: '/前端設置.html' },
+          { text: '資料庫設置', link: '/資料庫設置.html' },
+        ]
+      }
+	  ]
+  },
+  ignoreDeadLinks: 'localhostLinks'
+}
---
+++ docs-website/docs/index.md
@@ -0,0 +1,31 @@
+---
+layout: home
+
+title: 碳鍵
+titleTemplate: 次世代筆戰平台
+
+hero:
+  name: 碳鍵
+  text: 次世代筆戰平台
+  tagline: 圖像化掌握全盤戰局
+  image:
+    src: /icon.svg
+    alt: 碳鍵圖標
+    width: 192
+  actions:
+    - theme: brand
+      text: 出發
+      link: /概述/碳鍵是什麼
+    - theme: alt
+      text: 源碼
+      link: https://github.com/carbon-bond/carbonbond
+features:
+  - title: 鍵結
+    details: 以鍵結表現文章之間的關係（激賞、厭惡、提及……）
+  - title: 文章結構
+    details: 板主可設定看板的分類，使得分類的文章必須符合指定格式
+  - title: 匿名
+    details: 留言與發表文章時，可以選擇是否匿名
+  - title: 聊天室
+    details: 與有興趣板友的發起即時通訊
+---
