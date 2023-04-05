542699381bf8d57203aa0979b37d4d5eecc7695b
MROS <yc1043@gmail.com>
2023-04-05 18:33:51 +0800

    導出 webhook_type

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -1,4 +1,5 @@
 import * as api_trait from './api_trait';
 import * as api_utils from './api_utils';
+import * as webhook_type from './webhook_type';
 
-export { api_trait, api_utils };
+export { api_trait, api_utils, webhook_type };
